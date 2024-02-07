d7f13f343722be544a15f86a44136abaa3ca35e1
Muhammad Saqlain <135164284+msaqlain143@users.noreply.github.com>
2024-02-07 14:49:50 -0800

    Muhammad Saqlain

---
+++ app/Admin/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Admin\Controllers\StudentController;
+use App\Admin\Controllers\UserController;
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->resource('students', StudentController::class);
+    $router->resource('users', UserController::class);
+
+});
